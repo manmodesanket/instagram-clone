01f8843cd3891d0295cbc2a7a86e37320733a1a2
manmodesanket <manmodesanket@gmail.com>
2021-09-25 11:52:16 +0530

    marrgin chnage in post and follow on sidebar updated

--- src/component/Sidebar/Sidebar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import useUser from "../../hooks/use-user";
-
-export default function Sidebar() {
-  const { user: { docId, userId, following, username, fullName } = {} } =
-    useUser();
-  return <section className="hidden sm:block sm:4/12">Sidebar</section>;
-}
